4edc899ef044ba8450aa00d4e9b2339fa64af67e
도경 <dokyungkim2926@gmail.com>
2023-12-26 22:06:09 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.21"
-	kotlin("plugin.spring") version "1.9.21"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	id("org.springframework.boot") version "2.5.13"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("plugin.jpa") version "1.6.21"
+	id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
 group = "com.example"
@@ -19,9 +21,34 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	implementation("com.amazonaws:aws-java-sdk-s3:1.12.281")
+	compileOnly("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	implementation("mysql:mysql-connector-java:8.0.23")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.21")
+
+	implementation(enforcedPlatform("org.springframework.cloud:spring-cloud-dependencies:2020.0.5"))
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation ("org.springframework.cloud:spring-cloud-starter-loadbalancer")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+	implementation ("org.springdoc:springdoc-openapi-ui:1.6.11")
+
 }
 
 tasks.withType<KotlinCompile> {
