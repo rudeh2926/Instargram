fe8561cf73c130eb10d204594547505f940333ed
도경 <dokyungkim2926@gmail.com>
2023-12-26 20:08:46 +0900

    패키지구조 변경

---
+++ src/main/kotlin/com/example/instargram/InstargramApplication.kt
@@ -0,0 +1,11 @@
+package com.example.instargram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstargramApplication
+
+fun main(args: Array<String>) {
+	runApplication<InstargramApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/instargram/InstargramApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.instargram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstargramApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
