10d57646f96e60c0647b59736fe34b069de28c08
도경 <dokyungkim2926@gmail.com>
2023-12-26 19:42:08 +0900

    buildSrc 세팅

---
+++ instargram/instargram-buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,9 @@
+object DependencyVersions {
+    const val SPRING_CLOUD_VERSION = "2021.0.5"
+    const val KTLINT_VERSION = "0.44.0"
+    const val JWT_VERSION = "0.9.1"
+    const val AWS_VERSION = "2.2.6.RELEASE"
+    const val REDIS_VERSION = "2.7.2"
+    const val SPRING_TRANSACTION = "5.3.22"
+    const val QUERYDSL = "5.0.0"
+}
