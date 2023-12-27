147f58b3f9b3b4a1d148e574f5f3f9f6d3cb7d5c
도경 <dokyungkim2926@gmail.com>
2023-12-27 17:05:36 +0900

    회원가입

--- src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
@@ -1,4 +1,17 @@
 package com.example.instargram.domain.user.presentation
 
-class UserController {
+import com.example.instargram.domain.user.presentation.dto.request.SignupRequest
+import com.example.instargram.domain.user.service.UserSignupService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userSignupService: UserSignupService
+) {
+    
+    @PostMapping
+    fun signup(signupRequest: SignupRequest) =  userSignupService.execute(signupRequest)
 }
