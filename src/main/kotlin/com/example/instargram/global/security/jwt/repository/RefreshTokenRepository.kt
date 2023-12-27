b0b376f510ae6a366b75d67352ccec83243f2710
도경 <dokyungkim2926@gmail.com>
2023-12-27 11:32:32 +0900

    시큐리티 구성

--- src/main/kotlin/com/example/instargram/global/security/jwt/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/instargram/global/security/jwt/repository/RefreshTokenRepository.kt
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface RefreshTokenRepository: JpaRepository<RefreshToken, Long> {
-    fun existsByEmail(email : String) : Boolean
-    fun findByEmail(email : String) : Optional<RefreshToken>
 }
