1d189de0d236ddf4d5cd4a8c3fb7a3dcf3cf0f10
도경 <dokyungkim2926@gmail.com>
2023-12-31 23:10:48 +0900

    로그인 추가

--- src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
@@ -1,6 +1,8 @@
 package com.example.instargram.domain.user.presentation
 
+import com.example.instargram.domain.user.presentation.dto.request.LoginRequest
 import com.example.instargram.domain.user.presentation.dto.request.SignupRequest
+import com.example.instargram.domain.user.service.UserLoginService
 import com.example.instargram.domain.user.service.UserSignupService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val userSignupService: UserSignupService
+    private val userSignupService: UserSignupService,
+    private val userLoginService: UserLoginService
 ) {
 
     @PostMapping
-    fun signup(signupRequest: SignupRequest) =  userSignupService.execute(signupRequest)
+    fun signup(signupRequest: SignupRequest) =
+        userSignupService.execute(signupRequest)
+
+    @PostMapping("/login")
+    fun login(loginRequest: LoginRequest) =
+        userLoginService.execute(loginRequest)
 }
